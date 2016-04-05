36d2a2bad10eae384e44f14ccc7e0ff37675bc88
deostroll <deostroll@gmail.com>
2016-04-05 15:31:21 +0530

    figured use of CanActivate and OnActivate - they are to be used on routes

--- src/app/cmps/profile/profile.ts
+++ src/app/cmps/profile/profile.ts
@@ -1,8 +1,13 @@
 import {Component} from 'angular2/core';
+import {OnActivate, ComponentInstruction} from 'angular2/router';
 
 @Component({
   templateUrl:  'app/cmps/profile/profile.html'
 })
-export class Profile {
+export class Profile implements OnActivate {
   //This should cause error
+  routerOnActivate(n: ComponentInstruction, p: ComponentInstruction) {
+    console.log('OnActivate');
+    return false;
+  }
 }

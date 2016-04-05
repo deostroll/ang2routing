8fb920723414c815af8de2d722a7124c9d40cb4b
deostroll <deostroll@gmail.com>
2016-04-05 18:40:05 +0530

    fixed profile

--- src/app/cmps/profile/profile.ts
+++ src/app/cmps/profile/profile.ts
@@ -1,16 +1,23 @@
 import {Component} from 'angular2/core';
 import {OnActivate, ComponentInstruction} from 'angular2/router';
 import {NavSvc} from './../../svcs/NavSvc';
+import {RemSvc} from './../../svcs/RemSvc';
+
 @Component({
   templateUrl:  'app/cmps/profile/profile.html'
 })
 export class Profile implements OnActivate {
-  //This should cause error
-  constructor(private nav: NavSvc) {
+  pending: number;
+  total: number;
+  completed: number;
 
+  constructor(private nav: NavSvc, rem: RemSvc) {
+    this.pending = rem.reminders.filter( x => !x.isDone).length;
+    this.total = rem.reminders.length
+    this.completed = this.total - this.pending;    
   }
   routerOnActivate(n: ComponentInstruction, p: ComponentInstruction) {
     console.log('Profile OnActivate');
-    setTimeout(() => this.nav.navChange.emit('profile'), 100);    
+    setTimeout(() => this.nav.navChange.emit('profile'), 100);
   }
 }

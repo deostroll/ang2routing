da0d7f5866c824275cc99a688332e9434d7a5910
deostroll <deostroll@gmail.com>
2016-04-05 14:52:24 +0530

    commented canactivate and onactivate stuff in navigation

--- src/app/cmps/navs/navigation.ts
+++ src/app/cmps/navs/navigation.ts
@@ -1,13 +1,15 @@
 import {Component} from 'angular2/core';
-import {OnActivate, ROUTER_DIRECTIVES, CanActivate} from 'angular2/router';
+// import {OnActivate, ROUTER_DIRECTIVES, CanActivate} from 'angular2/router';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
   selector: 'navs',
   templateUrl: 'app/cmps/navs/links.html',
   directives: [ROUTER_DIRECTIVES]
 })
-export class Navigation implements CanActivate {
-  constructor(){
+
+export class Navigation {
+  constructor() {
     console.log('Navigation');
   }
 
@@ -19,11 +21,11 @@ export class Navigation implements CanActivate {
   //   console.log('reuse');
   // }
 
-  routerCanActivate() {
-    return true;
-  }
-
-  routerOnActivate(next: any, prev: any){
-    console.log('next:', next, 'prev:', prev);
-  }
+  // routerCanActivate() {
+  //   return true;
+  // }
+  //
+  // routerOnActivate(next: any, prev: any){
+  //   console.log('next:', next, 'prev:', prev);
+  // }
 }
